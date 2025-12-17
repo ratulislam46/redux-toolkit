9cd38cee1e410871287b10d7d10104b26d9e8d20
ratulislam46 <arfanratul46@gmail.com>
2025-12-17 11:23:12 +0600

    basic setup complete

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import TodosData from './components/todosData';
+
+function App() {
+
+  return (
+    <div className='todo-container'>
+      <TodosData />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/TodosData.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const todosData = () => {
+    return (
+        <div>
+            <h3>Todos Data Here</h3>
+        </div>
+    );
+};
+
+export default todosData;
